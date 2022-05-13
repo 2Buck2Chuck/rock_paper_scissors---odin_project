bdea1e38b2d96a3b2396d01f2e46cf388aef5f1d
2Buck2Chuck <charlesnhatch@gmail.com>
2022-05-13 22:36:40 +0200

    Add playerPlay function and game function
    - Add basic functionality to game function

--- rps.js
+++ rps.js
@@ -5,45 +5,57 @@ function computerPlay() {
     return choice;
 }
 
+function playerPlay() {
+   const choice = window.prompt("Please type ROCK, PAPER, or SCISSORS");
+   return choice;
+}
+
 function round(computerSelection, playerSelection) {
     if (computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "ROCK") {
-      let response = "You both chose ROCK. It's a tie!";
+      let response = alert("You both chose ROCK. It's a tie!", "Click any key to continue");
       return response;
     }
     else if (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase() === "PAPER") {
-        let response = "You both chose PAPER. It's a tie!";
+        let response = alert("You both chose PAPER. It's a tie!", "Click any key to continue");
         return response;
     }
     else if (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "SCISSORS") {
-        let response = "You both chose SCISSORS. It's a tie!";
+        let response = alert("You both chose SCISSORS. It's a tie!", "Click any key to continue");
         return response;
     }
     else if (computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "PAPER") {
-        let response = "PAPER beats ROCK! You win!";
+        let response = alert("PAPER beats ROCK! You win!", "Click any key to continue");
         return response;
     }
     else if (computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "SCISSORS") {
-        let response = "ROCK beats SCISSORS! You lose!";
+        let response = alert("ROCK beats SCISSORS! You lose!", "Click any key to continue");
         return response;
     }
     else if (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase() === "ROCK") {
-        let response = "PAPER beats ROCK! You lose!";
+        let response = alert("PAPER beats ROCK! You lose!", "Click any key to continue");
         return response;
     }
     else if (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase() === "SCISSORS") {
-        let response = "SCISSORS beats PAPER! You win!";
+        let response = alert("SCISSORS beats PAPER! You win!", "Click any key to continue");
         return response;
     }
     else if (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "PAPER") {
-        let response = "SCISSORS beats PAPER! You lose!";
+        let response = alert("SCISSORS beats PAPER! You lose!", "Click any key to continue");
         return response;
     }
     else if (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "ROCK") {
-        let response = "ROCK beats SCISSORS! You win!";
+        let response = alert("ROCK beats SCISSORS! You win!", "Click any key to continue");
         return response;
     }
     else {
-        return "Make a proper selection please!";
+        return prompt("Make a proper selection please!");
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        round(computerPlay(), playerPlay());
+     }
+}
+
+game();
