d69e41e2c08277cf0b43281e1bda6ec8d240ac6c
2Buck2Chuck <charlesnhatch@gmail.com>
2022-05-13 21:55:23 +0200

    Add last else statement to round and return statements to both functions
    - Also replace OR operators with AND operators in round

--- rps.js
+++ rps.js
@@ -1,36 +1,49 @@
-function computerPlay () {
+function computerPlay() {
     const rps = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * rps.length);
-    console.log(rps[random]);
+    let choice = rps[random];
+    return choice;
 }
 
-function round (computerSelection, playerSelection) {
-    if (computerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "ROCK") {
-      let response = "You both chose ROCK. It's a tie!"
+function round(computerSelection, playerSelection) {
+    if (computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "ROCK") {
+      let response = "You both chose ROCK. It's a tie!";
+      return response;
     }
-    else if (computerSelection.toUpperCase() === "PAPER" || playerSelection.toUpperCase() === "PAPER") {
-        let response = "You both chose PAPER. It's a tie!"
+    else if (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase() === "PAPER") {
+        let response = "You both chose PAPER. It's a tie!";
+        return response;
     }
-    else if (computerSelection.toUpperCase() === "SCISSORS" || playerSelection.toUpperCase() === "SCISSORS") {
-        let response = "You both chose SCISSORS. It's a tie!"
+    else if (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "SCISSORS") {
+        let response = "You both chose SCISSORS. It's a tie!";
+        return response;
     }
-    else if (computerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "PAPER") {
-        let response = "PAPER beats ROCK! You win!"
+    else if (computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "PAPER") {
+        let response = "PAPER beats ROCK! You win!";
+        return response;
     }
-    else if (computerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "SCISSORS") {
-        let response = "ROCK beats SCISSORS! You lose!"
+    else if (computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "SCISSORS") {
+        let response = "ROCK beats SCISSORS! You lose!";
+        return response;
     }
-    else if (computerSelection.toUpperCase() === "PAPER" || playerSelection.toUpperCase() === "ROCK") {
-        let response = "PAPER beats ROCK! You lose!"
+    else if (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase() === "ROCK") {
+        let response = "PAPER beats ROCK! You lose!";
+        return response;
     }
-    else if (computerSelection.toUpperCase() === "PAPER" || playerSelection.toUpperCase() === "SCISSORS") {
-        let response = "SCISSORS beats PAPER! You win!"
+    else if (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase() === "SCISSORS") {
+        let response = "SCISSORS beats PAPER! You win!";
+        return response;
     }
-    else if (computerSelection.toUpperCase() === "SCISSORS" || playerSelection.toUpperCase() === "PAPER") {
-        let response = "SCISSORS beats PAPER! You lose!"
+    else if (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "PAPER") {
+        let response = "SCISSORS beats PAPER! You lose!";
+        return response;
     }
-    else if (computerSelection.toUpperCase() === "SCISSORS" || playerSelection.toUpperCase() === "ROCK") {
-        let response = "ROCK beats SCISSORS! You win!"
+    else if (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "ROCK") {
+        let response = "ROCK beats SCISSORS! You win!";
+        return response;
     }
-
+    else {
+        return "Make a proper selection please!";
+    }
 }
+
